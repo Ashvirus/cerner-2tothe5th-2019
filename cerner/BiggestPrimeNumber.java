4eb49d8413e30db7c3ed545b4aca31cdad900443
Ashwini Singh <ars1546@rit.edu>
2019-09-16 10:45:54 -0400

    Update BiggestPrimeNumber.java

--- cerner/BiggestPrimeNumber.java
+++ cerner/BiggestPrimeNumber.java
@@ -4,27 +4,26 @@ import java.math.BigInteger;
 public class BiggestPrimeNumber {
 
 	public static void main(String[] args) {
-		printPrime();
+		BigInteger i;
+		BigInteger finalPrimeNum = BigInteger.ONE;
+		try {
+			for (i = BigInteger.valueOf((2 ^ 32) ^ Integer.MAX_VALUE);; i = i.add(BigInteger.ONE)) {
+				if (isPrime(i))
+					finalPrimeNum = i;
+			}
+		} catch (Exception e) {
+			System.out.println(finalPrimeNum);
+		}
 	}
 
 	private static boolean isPrime(BigInteger n) {
 		if (n.compareTo(BigInteger.ONE) <= 0)
 			return false;
 		BigInteger i;
-
-		BigInteger sqrt = n.pow(1 / 2);
-		for (i = BigInteger.valueOf(2); i.compareTo(sqrt) <= 0; i = i.add(BigInteger.ONE))
+		BigInteger half = n.divide(BigInteger.valueOf(2));
+		for (i = BigInteger.valueOf(2); i.compareTo(half) <= 0; i = i.add(BigInteger.ONE))
 			if (n.mod(i).equals(BigInteger.ZERO))
 				return false;
-
 		return true;
-	}
-
-	private static void printPrime() {
-		BigInteger i;
-		for (i = BigInteger.valueOf((2 ^ 32) ^ Integer.MAX_VALUE);; i = i.add(BigInteger.ONE)) {
-			if (isPrime(i))
-				System.out.println(i);
-		}
 	}
 }

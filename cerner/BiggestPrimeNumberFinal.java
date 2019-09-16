6afaab3c8d6e1255a80862e9a13d2f1791065b47
Ashwini Singh <ars1546@rit.edu>
2019-09-16 15:53:32 -0400

    Add files via upload

---
+++ cerner/BiggestPrimeNumberFinal.java
@@ -0,0 +1,29 @@
+//cerner_2^5_2019
+import java.math.BigInteger;
+
+public class BiggestPrimeNumberFinal {
+
+	public static void main(String[] args) {
+		BigInteger i;
+		BigInteger finalPrimeNum = BigInteger.ONE;
+		try {
+			for (i = BigInteger.valueOf((2 ^ 32) ^ Integer.MAX_VALUE);; i = i.add(BigInteger.ONE)) {
+				if (isPrime(i))
+					finalPrimeNum = i;
+			}
+		} catch (Exception e) {
+			System.out.println(finalPrimeNum);
+		}
+	}
+
+	private static boolean isPrime(BigInteger n) {
+		if (n.compareTo(BigInteger.ONE) <= 0)
+			return false;
+		BigInteger i;
+		BigInteger half = n.divide(BigInteger.valueOf(2));
+		for (i = BigInteger.valueOf(2); i.compareTo(half) <= 0; i = i.add(BigInteger.ONE))
+			if (n.mod(i).equals(BigInteger.ZERO))
+				return false;
+		return true;
+	}
+}
